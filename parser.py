f420fdeae3270eaf249c15369c1603c0a216b175
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-08-13 07:12:40 +1200

    Imported urllib.parse Added url to request and request arguments Added examples of GET and POST requests Added try/except for response processing

--- parser.py
+++ parser.py
@@ -1,4 +1,26 @@
-import urllib.request #standard library
+import urllib.request
+import urllib.parse
 
-x = urllib.request.urlopen('https://google.com/')
-print(x.read())
+# x = urllib.request.urlopen('https://google.com/')
+# print(x.read())
+
+# https://www.google.com/search?q=Monthy+Python
+
+url = 'https://google.com/search'
+get = {
+    'q' : 'Monthy Python'
+}
+
+
+data = urllib.parse.urlencode(get) # Encodes to url format
+data = data.encode('utf-8') # Converts to binary utf-8
+
+# req  = urllib.request.Request(url, data) # POST
+# req  = urllib.request.Request(url + '?' + data.decode("utf-8")) # GET
+
+try:
+    resp = urllib.request.urlopen(req)
+    print(resp.read())
+
+except Exception as e:
+    print(str(e))
