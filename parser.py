2cd9927128ce7e30fb3215c0e2971e3febb6df12
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-08-13 09:40:44 +1200

    Added headers example.

--- parser.py
+++ parser.py
@@ -11,16 +11,22 @@ get = {
     'q' : 'Monthy Python'
 }
 
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/43.0.2357.130 Chrome/43.0.2357.130 Safari/537.36'
+}
+
 
 data = urllib.parse.urlencode(get) # Encodes to url format
 data = data.encode('utf-8') # Converts to binary utf-8
 
 # req  = urllib.request.Request(url, data) # POST
-# req  = urllib.request.Request(url + '?' + data.decode("utf-8")) # GET
+req  = urllib.request.Request(url + '?' + data.decode("utf-8"), headers=headers) # GET
 
 try:
     resp = urllib.request.urlopen(req)
     print(resp.read())
 
 except Exception as e:
-    print(str(e))
+    print(str(e)) # 403 forbidden???
+                  # Google thinks that you are a robot...
+                  # ...because you do not have anything in HEADERS
