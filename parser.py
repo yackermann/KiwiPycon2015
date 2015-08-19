5aeefbe251c199b126f55458b5f92ba3815f54f6
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-08-19 21:09:27 +1200

    Finished parser

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import re
+import re, json
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
@@ -39,20 +39,32 @@ def clean( content, form ):
 def cook( content ):
     global soup
     soup = BeautifulSoup(content, 'html.parser')
+
+    def sortMixedVariable():
+        mixed = soup.find_all('div', { 'class' : 'details_block' })[0].text.replace('\n\n', '\n').replace(':\n', ':').split('\n')
+        toArray = [ item.split(':') for item in mixed if item != '' ]
+        metaInfo = {}
+        for item in toArray:
+            metaInfo[item[0].replace(' ', '').lower()] = item[1]
+        return metaInfo
+
+    mixed = sortMixedVariable()
+
     game = {
         'name'        : clean(soup.find_all('div', { 'class' : 'apphub_AppName' })[0].text, 'tabs'),
-        'price'       : clean(soup.find_all('div', { 'class' : "game_purchase_price price"})[0].text, 'numbers'),
+        'price'       : float(clean(soup.find_all('div', { 'class' : "game_purchase_price price"})[0].text, 'numbers')),
         'tags'        : [ clean(tag.text, 'tabs') for tag in soup.find_all('a', {'class' : 'app_tag'}) ],
-        'genre'       : '',
-        'publisher'   : '',
-        'releaseDate' : '',
+        'genre'       : mixed['genre'],
+        'publisher'   : mixed['publisher'],
+        'releasedate' : mixed['releasedate'],
         'rating'      : {
-            'count' : 0,
-            'total' : 0
+            'count' : float(clean(soup.find_all(attrs={ 'itemprop' : 'reviewCount'})[0]['content'], 'numbers')),
+            'total' : int(clean(soup.find_all(attrs={ 'itemprop' : 'ratingValue'})[0]['content'], 'numbers'))
         }
     }
 
-    print(game)
+    print(json.dumps(game, indent=4, separators=(',', ': ')))
+
 
 try:
     resp = urllib.request.urlopen(req)
