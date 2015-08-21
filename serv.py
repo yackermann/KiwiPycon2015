76d1b7990a0d7ee1f55a08c6a7de7e6bf3895fd5
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-08-21 12:44:14 +1200

    Added server

---
+++ serv.py
@@ -0,0 +1,78 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from tasklist import *
+import re, cgi
+
+man = TaskList()
+post = ''
+class HttpProcessor(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type','application/json')
+        self.end_headers()
+        
+        ip = self.client_address[0]
+
+        if(self.path == '/get'):
+            self.wfile.write(man.getTask(ip).encode('utf-8'))
+        else:
+            self.wfile.write('{}'.encode('utf-8'))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('content-type','application/json')
+        self.end_headers()
+
+        ip = self.client_address[0]
+
+        if self.path == '/update':
+
+            form = cgi.FieldStorage(
+                fp=self.rfile, 
+                headers=self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                        'CONTENT_TYPE':self.headers['Content-Type'],
+                        })
+
+            if 'method' in form:
+                method = form['method'].value
+                if method == 'update':
+                    man.updateStatus(ip)
+                    print('Success update')
+
+                elif method == 'data':
+                    if 'data' in form:
+                        data = form['data'].value
+                        try:
+                            d = json.loads(data)
+                            man.completeTask(ip, d)
+                            print('Success data')
+                        except:
+                            man.failTask(ip)
+                            print('Failed data')
+                    else:
+                        print('No data')
+
+                else:
+                     self.wfile.write('{"status" : 404}'.encode('utf-8'))
+
+
+
+
+        else:
+            self.wfile.write('{}'.encode('utf-8'))
+
+    def log_message(self, format, *args):
+        return
+
+
+was = 0
+step = 10
+for i in range(2500, 1000000, 2500):
+    id = str(was) + '-' + str(i)
+    man.addTask(id, range(was, i, step))
+    was = i
+
+print('Starting server...')
+
+serv = HTTPServer( ( 'localhost' , 8888 ) , HttpProcessor )
+serv.serve_forever()
---
+++ tasklist.py
@@ -0,0 +1,67 @@
+import json 
+from datetime import datetime as time
+class Task:
+    def __init__(self, id, r):
+        self.r  = r
+        self.id = id
+
+    def addData(self, data):
+        self.data = data
+
+    def getData(self):
+        return self.data
+
+    def setTimestamp(self):
+        self.timestamp = time.now()
+
+    def getTimestamp(self):
+        return self.timestamp
+
+    def json(self):
+        return json.dumps({
+            'id' : self.id,
+            'range': {
+                'start' : self.r.start,
+                'stop'  : self.r.stop,
+                'step'  : self.r.step
+            }
+        })
+
+class TaskList:
+    def __init__(self):
+        self.new = []
+        self.complete = []
+        self.running = {}
+
+    def addTask(self, id, r):
+        self.new.append(Task(id, r))
+
+    def getTask(self, ip):
+        if len(self.new) > 0 and ip not in self.running:
+            task = self.new.pop()
+            task.setTimestamp()
+            self.running[ip] = task
+            return task.json()
+        elif ip in self.running:
+            return self.running[ip].json()
+
+    def getJSON(self, ip):
+        if ip in self.running:
+            return self.running[ip].json()
+
+    def updateStatus(self, ip):
+        if ip in self.running:
+            self.running[ip].setTimestamp()
+
+    def failTask(self, ip):
+        if ip in self.running:
+            task = self.running[ip]
+            del self.running[ip]
+            self.new.append(task)
+
+    def completeTask(self, ip, data):
+        if ip in self.running:
+            task = self.running[ip]
+            task.addData(data)
+            del self.running[ip]
+            self.complete.append(task)
