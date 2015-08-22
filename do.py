c36132bf24213663dc69e560821ae2b86ed79c7a
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-08-22 17:03:51 +1200

    Added region change for do.

--- do.py
+++ do.py
@@ -7,27 +7,31 @@ token = os.getenv('DO_API_TOKEN', '')
 #Getting DO manager
 man = do.Manager(token=token)
 
+prefix = 'zergling'
 #Creating pool of vms
-def createPool():
-    for i in range( 0, 25 ):
-        name = 'insanity.' + str(i)
+def createPool(quantity=40, region='sfo1'):
+    for i in range( 0, quantity ):
+        name = prefix + '.' + region + '.' + str(i)
         droplet = do.Droplet(token = token,
                              name  = name,
-                             region = 'sfo1', # San Francisco 1
-                             image = 'ubuntu-14-04-x64',
+                             region = region, # San Francisco 1
+                             image = 13232378,
                              size_slug = '512mb', # Cheapest option
-                             backups = False)
+                             private_networking = True)
         droplet.create()
         print('Creating:', name)
 
 #Getting pool status
-def getDropletsStatus():
+def getDropletsStatus(region='sfo1'):
     for droplet in man.get_all_droplets():
-        if 'insanity.' in droplet.name:
+        if prefix + '.' + region in droplet.name:
             print(droplet.name, droplet.status)
 
-def destroyPool():
+def destroyPool(region='sfo1'):
     for droplet in man.get_all_droplets():
-        if 'insanity.' in droplet.name:
-            droplet.destroy()
-            print('Destroying: ' + droplet.name)
+        if prefix + '.' + region in droplet.name:
+            try:
+                droplet.destroy()
+                print('Destroying: ' + droplet.name)
+            except:
+                print('Can not destroy', droplet.name, 'Busy!')
