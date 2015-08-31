ae75079031739b08da36bae5af8db1b723118bd8
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-08-31 13:31:20 +1200

    Added docstrings, and shuffle on task fail.

--- tasklist.py
+++ tasklist.py
@@ -1,29 +1,42 @@
+"""TaskList class
+Yuriy Ackermann, 2015 
+ackermann.yuriy@gmail.com
+The MIT License (MIT)
+"""
+
 import json 
 from datetime import datetime as time
 class Task:
+    """Task Class"""
     def __init__(self, id, r):
         self.r    = r
         self.id   = id
         self.data = ''
 
     def addData(self, data):
+        """Assets Task data value"""
         self.data = data
         self.cache()
 
     def cache(self):
+        """Caches Task data"""
         with open('cache/' + str(self.id) + '.json', 'w') as cache:
             cache.write(json.dumps(self.data, indent=4, separators=(',', ': ')))
 
     def getData(self):
+        """Returns Tasks data"""
         return self.data
 
     def setTimestamp(self):
+        """Sets Task timestamp"""
         self.timestamp = time.now()
 
     def getTimestamp(self):
+        """Returns Task timestamp"""
         return self.timestamp
 
     def json(self):
+        """Returns JSON value encoded Task"""
         return json.dumps({
             'start' : self.r.start,
             'stop'  : self.r.stop,
@@ -31,15 +44,18 @@ class Task:
         })
 
 class TaskList:
+    """TaskList Class"""
     def __init__(self):
         self.new = []
         self.complete = []
         self.running = {}
 
     def addTask(self, id, r):
+        """Adds new Task to TaskList"""
         self.new.append(Task(id, r))
 
     def getTask(self, ip):
+        """Returns Task from TaskList, specified by ip address"""
         if len(self.new) > 0 and ip not in self.running:
             task = self.new.pop()
             task.setTimestamp()
@@ -53,22 +69,28 @@ class TaskList:
             return '{}'
 
     def getJSON(self, ip):
+        """Return JSON encoded Task, specified by ip address"""
         if ip in self.running:
             return self.running[ip].json()
         else:
             return '{}'
 
     def updateStatus(self, ip):
+        """Updates Task timestamp, specified by ip address"""
         if ip in self.running:
             self.running[ip].setTimestamp()
 
     def failTask(self, ip):
+        """Fails Task, specified by ip address"""
         if ip in self.running:
             task = self.running[ip]
+            lastTask = self.new.pop()
             self.new.append(task)
+            self.new.append(lastTask)
             del self.running[ip]
 
     def exportData(self):
+        """Exports all data to JSON file"""
         data = []
         filename = 'games.' + str(time.now()) + '.json'
         
@@ -84,6 +106,7 @@ class TaskList:
 
 
     def completeTask(self, ip, data):
+        """Completes task specified by ip address"""
         if ip in self.running:
             
             task = self.running[ip]
