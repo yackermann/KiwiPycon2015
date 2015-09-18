29ce6b44840a71655a40e683b8c3eaf65cea357e
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-09-18 22:21:34 +1200

    Renamed status pool function. Added cached droplet, region and images.

--- do.py
+++ do.py
@@ -10,6 +10,13 @@ man = do.Manager(token=token)
 # Pool Prefix
 prefix = 'zergling'
 
+droplets = {droplet.name: droplet for droplet in man.get_all_droplets()}
+regions  = sorted([region.slug for region in man.get_all_regions()])
+images   = { image.name : {
+                'id': image.id, 
+                'image': image 
+            } for image in man.get_all_images() }
+
 #Creating pool of vms
 def createPool(quantity=47, region='sfo1'):
     for i in range( 0, quantity ):
@@ -17,14 +24,14 @@ def createPool(quantity=47, region='sfo1'):
         droplet = do.Droplet(token = token,
                              name  = name,
                              region = region, # def San Francisco 1
-                             image = 13236931,
+                             image = '13236931',
                              size_slug = '512mb', # Cheapest option
                              private_networking = True)
         droplet.create()
         print('Creating:', name)
 
 #Getting pool status
-def getDropletsStatus(region='sfo1'):
+def getPoolStatus(region='sfo1'):
     for droplet in man.get_all_droplets():
         if prefix + '.' + region in droplet.name:
             print(droplet.name, droplet.status)
